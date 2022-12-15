45ce4e7550e14d7c0908cbb6e739a105ad346b97
nanderstabel <nanderstabel@gmail.com>
2022-12-15 16:45:36 +0100

    restructure

--- d07/src/main.rs
+++ d07/src/main.rs
@@ -5,6 +5,8 @@ use std::str::FromStr;
 const DISK_SIZE: usize = 70_000_000;
 const REQUIRED_DISK_SPACE: usize = 30_000_000;
 
+type List = Vec<Box<dyn FileType>>;
+
 #[derive(Debug)]
 enum Command {
     ChangeDirectory(Directory),
@@ -18,41 +20,19 @@ impl FromStr for Command {
         Ok(if let Some(("cd", arguments)) = s.split_once(' ') {
             Command::ChangeDirectory(arguments.parse()?)
         } else {
-            Command::List(s.parse()?)
-        })
-    }
-}
-
-impl FromStr for Directory {
-    type Err = ();
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        Ok(Directory::new(s.trim_end().to_string()))
-    }
-}
-
-#[derive(Debug)]
-struct List {
-    files: Vec<Box<dyn FileType>>,
-}
-
-impl FromStr for List {
-    type Err = ();
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        Ok(List {
-            files: s
-                .lines()
-                .skip(1)
-                .map(|l| {
-                    let (file_type, name) = l.split_once(' ').unwrap();
-                    let name = name.trim_end().to_string();
-                    match file_type {
-                        "dir" => Box::new(Directory::new(name)) as Box<dyn FileType>,
-                        size => Box::new(File::new(name, size.parse().unwrap())),
-                    }
-                })
-                .collect(),
+            Command::List(
+                s.lines()
+                    .skip(1)
+                    .map(|l| {
+                        let (file_type, name) = l.split_once(' ').unwrap();
+                        let name = name.trim_end().to_string();
+                        match file_type {
+                            "dir" => Box::new(Directory::new(name)) as Box<dyn FileType>,
+                            size => Box::new(File::new(name, size.parse().unwrap())),
+                        }
+                    })
+                    .collect(),
+            )
         })
     }
 }
@@ -63,12 +43,16 @@ trait FileType: std::fmt::Debug + FileTypeToAny {
     fn flatten(&self) -> Vec<&Directory>;
 }
 
-trait FileTypeToAny: 'static + Any {
-    fn as_any(&mut self) -> &mut dyn Any;
+trait FileTypeToAny: 'static {
+    fn as_any_mut(&mut self) -> &mut dyn Any;
+    fn as_any_ref(&self) -> &dyn Any;
 }
 
-impl<T: 'static + Any> FileTypeToAny for T {
-    fn as_any(&mut self) -> &mut dyn Any {
+impl<T: 'static> FileTypeToAny for T {
+    fn as_any_mut(&mut self) -> &mut dyn Any {
+        self
+    }
+    fn as_any_ref(&self) -> &dyn Any {
         self
     }
 }
@@ -85,10 +69,24 @@ impl File {
     }
 }
 
+impl FileType for File {
+    fn name(&self) -> String {
+        self.name.clone()
+    }
+
+    fn size(&self) -> usize {
+        self.size
+    }
+
+    fn flatten(&self) -> Vec<&Directory> {
+        vec![]
+    }
+}
+
 #[derive(Debug)]
 struct Directory {
     name: String,
-    children: Vec<Box<dyn FileType>>,
+    children: List,
 }
 
 impl Directory {
@@ -97,10 +95,6 @@ impl Directory {
             name,
             children: vec![],
         }
-    }
-
-    fn extend(&mut self, list: List) {
-        self.children.extend(list.files.into_iter())
     }
 
     pub fn ingest<I: IntoIterator<Item = Command>>(
@@ -112,13 +106,14 @@ impl Directory {
             match command {
                 Command::ChangeDirectory(cd) => {
                     if cd.name() != *".." {
-                        let index = self
+                        input = if let Some(dir) = (**self
                             .children
-                            .iter()
-                            .position(|r| r.name() == cd.name())
-                            .unwrap();
-                        let it: &mut dyn Any = (*self.children[index]).as_any();
-                        input = if let Some(dir) = it.downcast_mut::<Directory>() {
+                            .iter_mut()
+                            .find(|c| c.name() == cd.name())
+                            .unwrap())
+                        .as_any_mut()
+                        .downcast_mut::<Directory>()
+                        {
                             dir.ingest(input)
                         } else {
                             input
@@ -127,7 +122,7 @@ impl Directory {
                         break;
                     }
                 }
-                Command::List(ls) => self.extend(ls),
+                Command::List(ls) => self.children.extend(ls.into_iter()),
             }
         }
         input
@@ -157,22 +152,21 @@ impl FileType for Directory {
     fn flatten(&self) -> Vec<&Directory> {
         [
             vec![self],
-            self.children.iter().flat_map(|c| c.flatten()).collect(),
+            self.children
+                .iter()
+                .filter_map(|c| (**c).as_any_ref().downcast_ref::<Directory>())
+                .flat_map(|dir| dir.flatten())
+                .collect(),
         ]
         .concat()
     }
 }
-impl FileType for File {
-    fn name(&self) -> String {
-        self.name.clone()
-    }
 
-    fn size(&self) -> usize {
-        self.size
-    }
+impl FromStr for Directory {
+    type Err = ();
 
-    fn flatten(&self) -> Vec<&Directory> {
-        vec![]
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Ok(Directory::new(s.trim_end().to_string()))
     }
 }
 
@@ -214,21 +208,13 @@ fn main() {
 
     output!(
         root.iter()
-            .filter_map(|d| if d.size() <= 100000 {
-                Some(d.size())
-            } else {
-                None
-            })
+            .filter_map(|d| (d.size() <= 100000).then_some(d.size()))
             .sum::<usize>(),
         root.iter()
             .filter_map(|d| {
-                if d.size() as isize - (REQUIRED_DISK_SPACE - (DISK_SIZE - root.size())) as isize
-                    >= 0
-                {
-                    Some(d.size())
-                } else {
-                    None
-                }
+                (d.size() as isize - (REQUIRED_DISK_SPACE - (DISK_SIZE - root.size())) as isize
+                    >= 0)
+                    .then_some(d.size())
             })
             .min()
             .unwrap()
